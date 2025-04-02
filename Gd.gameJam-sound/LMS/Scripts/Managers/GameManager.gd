d0057f97dfecc09f5fc4c162c01799479787c05f
Declan <declan@raindropme.com>
2025-04-03 02:58:34 +0930

    + Cleared level resource for web upload testing + Fixed bug where pressing escape in the menu after returning from a level would open the pause menu + Changed the colour of the button font when pressed

--- Gd.gameJam-sound/LMS/Scripts/Managers/GameManager.gd
+++ Gd.gameJam-sound/LMS/Scripts/Managers/GameManager.gd
@@ -50,7 +50,7 @@ func _ready() -> void:
 	AudioHandler.CurrentTrackFinished.connect(_OnAudioTrackFinished)
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("Pause"):
+	if StateManager.IsGameplay() && event.is_action_pressed("Pause"):
 		Paused = !Paused
 
 func _OnLevelLoaded() -> void:
@@ -58,6 +58,7 @@ func _OnLevelLoaded() -> void:
 
 func _OnLevelQuit() -> void:
 	MenuManager.OpenMenu("LevelSelect")
+	#StateManager.GameState = StateManager.States.MENU
 
 func _OnMenusClosed() -> void:
 	Paused = false
